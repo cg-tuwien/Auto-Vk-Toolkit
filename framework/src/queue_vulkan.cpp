22a94fe658d08a0a3fad86b6a26a1bc5296efd04
Johannes Unterguggenberger <johannes.unterguggenberger@gmail.com>
2020-06-19 19:21:06 +0200

    queue w.i.p. Not sure how to handle it exactly yet. Currently, we need a replacement for `queue::create` (which is still in queue_vulkan)

--- framework/src/queue_vulkan.cpp
+++ framework/src/queue_vulkan.cpp
@@ -2,65 +2,6 @@
 
 namespace cgb
 {
-	std::deque<device_queue> device_queue::sPreparedQueues;
-
-	device_queue* device_queue::prepare(
-		vk::QueueFlags aFlagsRequired,
-		device_queue_selection_strategy aSelectionStrategy,
-		std::optional<vk::SurfaceKHR> aSupportForSurface)
-	{
-		auto families = context().find_best_queue_family_for(aFlagsRequired, aSelectionStrategy, aSupportForSurface);
-		if (families.size() == 0) {
-			throw cgb::runtime_error("Couldn't find queue families satisfying the given criteria.");
-		}
-
-		// Default to the first ones, each
-		uint32_t familyIndex = std::get<0>(families[0]);
-		uint32_t queueIndex = 0;
-
-		for (auto& family : families) {
-			for (uint32_t qi = 0; qi < std::get<1>(family).queueCount; ++qi) {
-
-				auto alreadyInUse = std::find_if(
-					std::begin(sPreparedQueues), 
-					std::end(sPreparedQueues), 
-					[lFamilyIndexInQuestion = std::get<0>(family), lQueueIndexInQuestion = qi](const auto& pq) {
-					return pq.family_index() == lFamilyIndexInQuestion
-						&& pq.queue_index() == lQueueIndexInQuestion;
-				});
-
-				// Pay attention to different selection strategies:
-				switch (aSelectionStrategy)
-				{
-				case cgb::device_queue_selection_strategy::prefer_separate_queues:
-					if (sPreparedQueues.end() == alreadyInUse) {
-						// didn't find combination, that's good
-						familyIndex = std::get<0>(family);
-						queueIndex = qi;
-						goto found_indices;
-					}
-					break;
-				case cgb::device_queue_selection_strategy::prefer_everything_on_single_queue:
-					if (sPreparedQueues.end() != alreadyInUse) {
-						// find combination, that's good in this case
-						familyIndex = std::get<0>(family);
-						queueIndex = 0; // => 0 ... i.e. everything on queue #0
-						goto found_indices;
-					}
-					break;
-				}
-			}
-		}
-
-	found_indices:
-		auto& prepd_queue = sPreparedQueues.emplace_back();
-		prepd_queue.mQueueFamilyIndex = familyIndex;
-		prepd_queue.mQueueIndex = queueIndex;
-		prepd_queue.mPriority = 0.5f; // default priority of 0.5
-		prepd_queue.mQueue = nullptr;
-		return &prepd_queue;
-	}
-
 	device_queue device_queue::create(uint32_t aQueueFamilyIndex, uint32_t aQueueIndex)
 	{
 		device_queue result;
