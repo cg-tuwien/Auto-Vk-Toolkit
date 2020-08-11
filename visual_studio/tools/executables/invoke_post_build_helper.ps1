b898d441841746da906d6461a5ad42aadf8d704d
Johannes Unterguggenberger <johannes.unterguggenberger@gmail.com>
2020-08-11 09:59:17 +0200

    - More useful output from invoke_post_build_helper.ps1 - Deleted Hardcodet.Wpf.TaskbarNotification.dll so that it really gets ignored

--- visual_studio/tools/executables/invoke_post_build_helper.ps1
+++ visual_studio/tools/executables/invoke_post_build_helper.ps1
@@ -9,8 +9,10 @@ if (-Not (Test-Path $postBuildExePath))
 		try 
 		{
 			$msbuildPath = Join-Path -Path $args[1] -ChildPath "MSBuild.exe" 
-			$postBuildSlnPath = Join-Path -Path $PSScriptRoot -ChildPath "../sources/cgb_post_build_helper/cgb_post_build_helper.sln"
+			$postBuildSlnPath = Join-Path -Path $PSScriptRoot -ChildPath "..\sources\cgb_post_build_helper\cgb_post_build_helper.sln"
 			Write-Output "$exeName not found => going to build it from $postBuildSlnPath ..."
+			Write-Output "Note 1: If the build takes too long (more than 10 sec.), please cancel it (Build -> Cancel) and build again."
+			Write-Output "Note 2: If the build fails, please open '$postBuildSlnPath' and build the C# project manually (just select the 'Release' configuration and build)."
 			Start-Process -Wait -WindowStyle Hidden -FilePath $msbuildPath -ArgumentList $postBuildSlnPath, '/property:Configuration=Release'
 		}
 		catch 
@@ -26,6 +28,11 @@ if (-Not (Test-Path $postBuildExePath))
 			$srcExe = Join-Path -Path $PSScriptRoot -ChildPath "fallback_cgb_post_build_helper.exe"
 			Write-Output "Building $exeName failed => going to copy it from $srcExe ..."
 			Copy-Item $srcExe -Destination $postBuildExePath
+
+			$srcDll = Join-Path -Path $PSScriptRoot -ChildPath "fallback.Hardcodet.Wpf.TaskbarNotification.dll"
+			$dstDll = Join-Path -Path $PSScriptRoot -ChildPath "Hardcodet.Wpf.TaskbarNotification.dll"
+			Write-Output "Also going to copy Hardcodet.Wpf.TaskbarNotification.dll from $srcDll ..."
+			Copy-Item $srcDll -Destination $dstDll
 		}
 		catch 
 		{
@@ -43,8 +50,9 @@ if (-Not (Test-Path $postBuildExePath))
 # Invoke the post build helper tool:
 Start-Process -FilePath $postBuildExePath -ArgumentList $args -WorkingDirectory $PSScriptRoot
 
+Write-Output "Post build helper has been invoked. Now waiting for the deployment of assets and shaders..."
 $maxLoop = 10
-for ($i=1; $i -le $maxLoop; $i++)
+for ($i=0; $i -le $maxLoop; $i++)
 {
 	$pipe = new-object System.IO.Pipes.NamedPipeClientStream '.','Gears-Vk Application Status Pipe','In'
 	$pipe.Connect()
@@ -58,7 +66,7 @@ for ($i=1; $i -le $maxLoop; $i++)
 		break
 	}
 
-	Write-Output $messageReceived
+	Write-Output "After $i sec.: $messageReceived"
 	Start-Sleep -s 1
 
 	if ($i -eq $maxLoop) 
