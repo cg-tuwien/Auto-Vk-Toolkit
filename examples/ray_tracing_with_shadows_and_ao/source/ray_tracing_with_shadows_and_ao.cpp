e1bbf21a3ef31354d95c8e630c0545abf5b28ee2
Johannes Unterguggenberger <johannes.unterguggenberger@gmail.com>
2021-05-10 15:20:55 +0200

    Restructured `ray_query_in_ray_tracing_shaders` and `ray_tracing_with_shadows_and_ao` into two `invokee`s for a nicer code structure.

--- examples/ray_tracing_with_shadows_and_ao/source/ray_tracing_with_shadows_and_ao.cpp
+++ examples/ray_tracing_with_shadows_and_ao/source/ray_tracing_with_shadows_and_ao.cpp
@@ -1,6 +1,14 @@
 #include <gvk.hpp>
 #include <imgui.h>
-#include <imgui_internal.h>
+#include "triangle_mesh_geometry_manager.hpp"
+
+// Set this compiler switch to 1 to enable hot reloading of
+// the ray tracing pipeline's shaders. Set to 0 to disable it.
+#define ENABLE_SHADER_HOT_RELOADING_FOR_RAY_TRACING_PIPELINE 1
+
+// Set this compiler switch to 1 to make the window resizable
+// and have the pipeline adapt to it. Set to 0 ti disable it.
+#define ENABLE_RESIZABLE_WINDOW 1
 
 // Data to be pushed to the GPU along with a specific draw call:
 struct push_const_data {
@@ -19,18 +27,10 @@ struct push_const_data {
 	glm::vec4  mAmbientOcclusionColor;
 };
 
-// Set this compiler switch to 1 to enable hot reloading of
-// the ray tracing pipeline's shaders. Set to 0 to disable it.
-#define ENABLE_SHADER_HOT_RELOADING_FOR_RAY_TRACING_PIPELINE 1
-
-// Set this compiler switch to 1 to make the window resizable
-// and have the pipeline adapt to it. Set to 0 ti disable it.
-#define ENABLE_RESIZABLE_WINDOW 1
-
 // Main invokee of this application:
 class ray_tracing_with_shadows_and_ao_invokee : public gvk::invokee
 {
-public: // v== xk::invokee overrides which will be invoked by the framework ==v
+public: // v== gvk::invokee overrides which will be invoked by the framework ==v
 	ray_tracing_with_shadows_and_ao_invokee(avk::queue& aQueue)
 		: mQueue{ &aQueue }
 	{}
@@ -47,110 +47,6 @@ public: // v== xk::invokee overrides which will be invoked by the framework ==v
 		// Get a pointer to the main window:		
 		auto* mainWnd = gvk::context().main_window();
 
-		// Load an ORCA scene from file:
-		auto orca = gvk::orca_scene_t::load_from_file("assets/sponza_and_terrain.fscene", aiProcess_Triangulate | aiProcess_GenSmoothNormals | aiProcess_CalcTangentSpace);
-
-		// Prepare a vector to hold all the material information of all models:
-		std::vector<gvk::material_config> materialData;
-
-		for (auto& model : orca->models()) {
-			auto& nameAndRangeInfo = mBlasNamesAndRanges.emplace_back(
-				model.mName,                                  // We are about to add several entries to mBlas for the model with this name
-				static_cast<int>(mGeometryInstances.size()),  // These ^ entries start at this index
-				-1                                            //  ... and we'll figure out the end index as we go.
-			);
-
-			// Get the distinct materials for every (static) mesh and accumulate them in a big array
-			// wich will be transformed and stored in a big buffer, eventually:
-			auto distinctMaterials = model.mLoadedModel->distinct_material_configs();
-			for (const auto& [materialConfig, meshIndices] : distinctMaterials) {
-				materialData.push_back(materialConfig);
-
-				auto selection = gvk::make_models_and_meshes_selection(model.mLoadedModel, meshIndices);
-				// Store all of this data in buffers and buffer views, s.t. we can access it later in ray tracing shaders
-				auto [posBfr, idxBfr] = gvk::create_vertex_and_index_buffers<avk::uniform_texel_buffer_meta>(
-					selection,                                          // Select several indices (those with the same material) from a model
-					vk::BufferUsageFlagBits::eShaderDeviceAddressKHR    // Buffers need this additional flag to be made usable with ray tracing
-					);
-				auto nrmBfr = gvk::create_normals_buffer               <avk::uniform_texel_buffer_meta>(selection);
-				auto texBfr = gvk::create_2d_texture_coordinates_buffer<avk::uniform_texel_buffer_meta>(selection);
-
-				// Create a bottom level acceleration structure instance with this geometry.
-				auto blas = gvk::context().create_bottom_level_acceleration_structure(
-					{ avk::acceleration_structure_size_requirements::from_buffers(avk::vertex_index_buffer_pair{ posBfr, idxBfr }) },
-					false // no need to allow updates for static geometry
-				);
-				blas->build({ avk::vertex_index_buffer_pair{ posBfr, idxBfr } });
-
-				// Create a geometry instance entry per instance in the ORCA scene file:
-				for (const auto& inst : model.mInstances) {
-					auto bufferViewIndex = static_cast<uint32_t>(mTexCoordsBufferViews.size());
-
-					// Create a concrete geometry instance:
-					mGeometryInstances.push_back(
-						gvk::context().create_geometry_instance(blas) // Refer to the concrete BLAS
-							// Set this instance's transformation matrix:
-						.set_transform_column_major(gvk::to_array(gvk::matrix_from_transforms(inst.mTranslation, glm::quat(inst.mRotation), inst.mScaling)))
-						// Set this instance's custom index, which is especially important since we'll use it in shaders
-						// to refer to the right material and also vertex data (these two are aligned index-wise):
-						.set_custom_index(bufferViewIndex)
-					);
-
-					// State that this geometry instance shall be included in TLAS generation by default:
-					mGeometryInstanceActive.push_back(true);
-
-					// Generate and store a description for what this geometry instance entry represents:
-					assert(!selection.empty());
-					std::string description = model.mName + " (" + inst.mName + "): "; // ...refers to one specific model, and...
-					for (const auto& tpl : selection) {
-						for (const auto meshIndex : std::get<1>(tpl)) {
-							description += std::get<0>(tpl)->name_of_mesh(meshIndex) + ", "; // ...to one or multiple submeshes.
-						}
-					}
-					mGeometryInstanceDescriptions.push_back(description.substr(0, description.size() - 2));
-				}
-
-				mBlas.push_back(std::move(blas)); // Move this BLAS s.t. we don't have to enable_shared_ownership. We're done with it here.
-
-				// After we have used positions and indices for building the BLAS, still need to create buffer views which allow us to access
-				// the per vertex data in ray tracing shaders, where they will be accessible via samplerBuffer- or usamplerBuffer-type uniforms.
-				mPositionsBufferViews.push_back(gvk::context().create_buffer_view(avk::owned(posBfr))); // owned is equivalent to move
-				mIndexBufferViews.push_back(gvk::context().create_buffer_view(avk::owned(idxBfr)));
-				mNormalsBufferViews.push_back(gvk::context().create_buffer_view(avk::owned(nrmBfr)));
-				mTexCoordsBufferViews.push_back(gvk::context().create_buffer_view(avk::owned(texBfr)));
-			}
-
-			// Set the final range-to index (one after the end, i.e. excluding the last index):
-			std::get<2>(nameAndRangeInfo) = static_cast<int>(mGeometryInstances.size());
-		}
-
-		// Convert the materials that were gathered above into a GPU-compatible format and generate and upload images to the GPU:
-		auto [gpuMaterials, imageSamplers] = gvk::convert_for_gpu_usage<gvk::material_gpu_data>(
-			materialData, true /* assume textures in sRGB */, true /* flip textures */,
-			avk::image_usage::general_texture,
-			avk::filter_mode::trilinear // No need for MIP-mapping (which would be activated with trilinear or anisotropic) since we're using ray tracing
-			);
-
-		// Store images in a member variable, otherwise they would get destroyed.
-		mImageSamplers = std::move(imageSamplers);
-
-		// Upload materials in GPU-compatible format into a GPU storage buffer:
-		mMaterialBuffer = gvk::context().create_buffer(
-			avk::memory_usage::host_visible, {},
-			avk::storage_buffer_meta::create_from_data(gpuMaterials)
-		);
-		mMaterialBuffer->fill(
-			gpuMaterials.data(), 0,
-			avk::sync::with_barriers(gvk::context().main_window()->command_buffer_lifetime_handler())
-		);
-
-		// Build the top-level acceleration structure
-		mTlas = gvk::context().create_top_level_acceleration_structure(
-			static_cast<uint32_t>(mGeometryInstances.size()), // Specify how many geometry instances there are expected to be
-			true                                              // Allow updates since we want to have the opportunity to enable/disable some of them via the UI
-		);
-		mTlas->build(mGeometryInstances);
-
 		// Create an offscreen image to ray-trace into. It is accessed via an image view:
 		const auto wdth = gvk::context().main_window()->resolution().x;
 		const auto hght = gvk::context().main_window()->resolution().y;
@@ -158,6 +54,17 @@ public: // v== xk::invokee overrides which will be invoked by the framework ==v
 		auto offscreenImage = gvk::context().create_image(wdth, hght, frmt, 1, avk::memory_usage::device, avk::image_usage::general_storage_image);
 		offscreenImage->transition_to_layout();
 		mOffscreenImageView = gvk::context().create_image_view(avk::owned(offscreenImage));
+
+		// The triangle_mesh_geometry_manager has lower execution order. Therefore, we can
+		// assume that it already contains the data that we require:
+		auto* triMeshGeomMgr = gvk::current_composition()->element_by_type<triangle_mesh_geometry_manager>();
+		assert(nullptr != triMeshGeomMgr);
+
+		// Initialize the TLAS (but don't build it yet)
+		mTlas = gvk::context().create_top_level_acceleration_structure(
+			triMeshGeomMgr->max_number_of_geometry_instances(), // <-- Specify how many geometry instances there are expected to be
+			true               // <-- Allow updates since we want to have the opportunity to enable/disable some of them via the UI
+		);
 
 		// Create our ray tracing pipeline with the required configuration:
 		mPipeline = gvk::context().create_ray_tracing_pipeline_for(
@@ -177,11 +84,11 @@ public: // v== xk::invokee overrides which will be invoked by the framework ==v
 			gvk::context().get_max_ray_tracing_recursion_depth(),
 			// Define push constants and descriptor bindings:
 			avk::push_constant_binding_data{ avk::shader_type::ray_generation | avk::shader_type::closest_hit, 0, sizeof(push_const_data) },
-			avk::descriptor_binding(0, 0, mImageSamplers),
-			avk::descriptor_binding(0, 1, mMaterialBuffer),
-			avk::descriptor_binding(0, 2, avk::as_uniform_texel_buffer_views(mIndexBufferViews)),
-			avk::descriptor_binding(0, 3, avk::as_uniform_texel_buffer_views(mTexCoordsBufferViews)),
-			avk::descriptor_binding(0, 4, avk::as_uniform_texel_buffer_views(mNormalsBufferViews)),
+			avk::descriptor_binding(0, 0, triMeshGeomMgr->image_samplers()),
+			avk::descriptor_binding(0, 1, triMeshGeomMgr->material_buffer()),
+			avk::descriptor_binding(0, 2, avk::as_uniform_texel_buffer_views(triMeshGeomMgr->index_buffer_views())),
+			avk::descriptor_binding(0, 3, avk::as_uniform_texel_buffer_views(triMeshGeomMgr->tex_coords_buffer_views())),
+			avk::descriptor_binding(0, 4, avk::as_uniform_texel_buffer_views(triMeshGeomMgr->normals_buffer_views())),
 			avk::descriptor_binding(1, 0, mOffscreenImageView->as_storage_image()), // Bind the offscreen image to render into as storage image
 			avk::descriptor_binding(2, 0, mTlas)                                    // Bind the TLAS, s.t. we can trace rays against it
 		);
@@ -220,7 +127,9 @@ public: // v== xk::invokee overrides which will be invoked by the framework ==v
 		if (nullptr != imguiManager) {
 			imguiManager->add_callback([this]() {
 				ImGui::Begin("Info & Settings");
-				ImGui::SetWindowPos(ImVec2(1.0f, 1.0f), ImGuiCond_FirstUseEver);
+				ImGui::SetWindowPos(ImVec2(10.0f, 10.0f), ImGuiCond_FirstUseEver);
+				ImGui::SetWindowSize(ImVec2(400.0f, 410.0f), ImGuiCond_FirstUseEver);
+
 				ImGui::Text("%.3f ms/frame", 1000.0f / ImGui::GetIO().Framerate);
 				ImGui::Text("%.1f FPS", ImGui::GetIO().Framerate);
 				ImGui::TextColored(ImVec4(0.f, .6f, .8f, 1.f), "[F1]: Toggle input-mode");
@@ -254,48 +163,20 @@ public: // v== xk::invokee overrides which will be invoked by the framework ==v
 					ImGui::ColorEdit3("AO Color", glm::value_ptr(mAmbientOcclusionColor));
 				}
 
-				ImGui::Separator();
-				if (ImGui::CollapsingHeader("Geometry Instances included in TLAS")) {
-					// Let the user enable/disable some geometry instances w.r.t. includion into the TLAS:
-					assert(mGeometryInstances.size() == mGeometryInstanceActive.size());
-					assert(mGeometryInstances.size() == mGeometryInstanceDescriptions.size());
-					auto numActive = std::accumulate(std::begin(mGeometryInstanceActive), std::end(mGeometryInstanceActive), 0, [](auto cur, auto nxt) { return cur + (nxt ? 1 : 0); });
-					for (size_t i = 0; i < mGeometryInstances.size(); ++i) {
-						bool tmp = mGeometryInstanceActive[i];
-
-						// If there is only one active element left, it may not be disabled:
-						const bool disable = numActive <= 1 && tmp;
-						if (disable) {
-							ImGui::PushItemFlag(ImGuiItemFlags_Disabled, true);
-						}
-
-						mTlasUpdateRequired = ImGui::Checkbox((mGeometryInstanceDescriptions[i] + "##geominst" + std::to_string(i)).c_str(), &tmp) || mTlasUpdateRequired;
-						mGeometryInstanceActive[i] = tmp;
-
-						if (disable) {
-							ImGui::PopItemFlag();
-						}
-					}
-				}
-
 				ImGui::End();
-				});
+			});
 		}
 	}
 
 	void update() override
 	{
-		if (mTlasUpdateRequired)
+		auto* triMeshGeomMgr = gvk::current_composition()->element_by_type<triangle_mesh_geometry_manager>();
+		assert(nullptr != triMeshGeomMgr);
+		if (triMeshGeomMgr->has_updated_geometry_for_tlas())
 		{
 			// Getometry selection has changed => rebuild the TLAS:
 
-			std::vector<avk::geometry_instance> activeGeometryInstances;
-			assert(mGeometryInstances.size() == mGeometryInstanceActive.size());
-			for (size_t i = 0; i < mGeometryInstances.size(); ++i) {
-				if (mGeometryInstanceActive[i]) {
-					activeGeometryInstances.push_back(mGeometryInstances[i]);
-				}
-			}
+			std::vector<avk::geometry_instance> activeGeometryInstances = triMeshGeomMgr->get_active_geometry_instances_for_tlas_build();
 
 			if (!activeGeometryInstances.empty()) {
 				auto& commandPool = gvk::context().get_command_pool_for_single_use_command_buffers(*mQueue);
@@ -360,13 +241,17 @@ public: // v== xk::invokee overrides which will be invoked by the framework ==v
 		auto& commandPool = gvk::context().get_command_pool_for_single_use_command_buffers(*mQueue);
 		auto cmdbfr = commandPool->alloc_command_buffer(vk::CommandBufferUsageFlagBits::eOneTimeSubmit);
 		cmdbfr->begin_recording();
+
+		// The triangle_mesh_geometry_manager has some of the data we require:
+		auto* triMeshGeomMgr = gvk::current_composition()->element_by_type<triangle_mesh_geometry_manager>();
+
 		cmdbfr->bind_pipeline(avk::const_referenced(mPipeline));
 		cmdbfr->bind_descriptors(mPipeline->layout(), mDescriptorCache.get_or_create_descriptor_sets({
-			avk::descriptor_binding(0, 0, mImageSamplers),
-			avk::descriptor_binding(0, 1, mMaterialBuffer),
-			avk::descriptor_binding(0, 2, avk::as_uniform_texel_buffer_views(mIndexBufferViews)),
-			avk::descriptor_binding(0, 3, avk::as_uniform_texel_buffer_views(mTexCoordsBufferViews)),
-			avk::descriptor_binding(0, 4, avk::as_uniform_texel_buffer_views(mNormalsBufferViews)),
+			avk::descriptor_binding(0, 0, triMeshGeomMgr->image_samplers()),
+			avk::descriptor_binding(0, 1, triMeshGeomMgr->material_buffer()),
+			avk::descriptor_binding(0, 2, avk::as_uniform_texel_buffer_views(triMeshGeomMgr->index_buffer_views())),
+			avk::descriptor_binding(0, 3, avk::as_uniform_texel_buffer_views(triMeshGeomMgr->tex_coords_buffer_views())),
+			avk::descriptor_binding(0, 4, avk::as_uniform_texel_buffer_views(triMeshGeomMgr->normals_buffer_views())),
 			avk::descriptor_binding(1, 0, mOffscreenImageView->as_storage_image()),
 			avk::descriptor_binding(2, 0, mTlas)
 			}));
@@ -445,42 +330,7 @@ private: // v== Member variables ==v
 	// The direction of our single light source, which is a directional light:
 	glm::vec3 mLightDir = { 0.0f, -1.0f, 0.0f };
 
-	// A buffer that stores all material data of the loaded models:
-	avk::buffer mMaterialBuffer;
-
-	// Several images(+samplers) which store the material data's images:
-	std::vector<avk::image_sampler> mImageSamplers;
-
-	// Buffer views which provide the indexed geometry's index data:
-	std::vector<avk::buffer_view> mIndexBufferViews;
-
-	// Buffer views which provide the indexed geometry's positions data:
-	std::vector<avk::buffer_view> mPositionsBufferViews;
-
-	// Buffer views which provide the indexed geometry's texture coordinates data:
-	std::vector<avk::buffer_view> mTexCoordsBufferViews;
-
-	// Buffer views which provide the indexed geometry's normals data:
-	std::vector<avk::buffer_view> mNormalsBufferViews;
-
 	// ----------- Resources required for ray tracing -----------
-
-	// A vector which stores a model name and a range of indices, refering to the mBlas vector.
-	// The indices referred to by the [std:get<1>, std::get<2>) range are the associated submeshes.
-	std::vector<std::tuple<std::string, int, int>> mBlasNamesAndRanges;
-
-	// A vector of multiple bottom-level acceleration structures (BLAS) which store geometry:
-	std::vector<avk::bottom_level_acceleration_structure> mBlas;
-
-	// Geometry instance data which store the instance data per BLAS inststance:
-	//    In our specific setup, this will be perfectly aligned with:
-	//     - mIndexBufferViews
-	//     - mTexCoordBufferViews
-	//     - mNormalsBufferViews
-	std::vector<avk::geometry_instance> mGeometryInstances;
-
-	// A description per geometry instance to roughly describe what they refer to:
-	std::vector<std::string> mGeometryInstanceDescriptions;
 
 	// We are using one single top-level acceleration structure (TLAS) to keep things simple:
 	//    (We're not duplicating the TLAS per frame in flight. Instead, we
@@ -539,10 +389,12 @@ int main() // <== Starting point ==
 		mainWnd->set_present_queue(singleQueue);
 		// ... pass the queue to the constructors of the invokees:
 
-		// Create an instance of our main avk::invokee which will perform the initial setup and spawn further invokees:
-		auto app = ray_tracing_with_shadows_and_ao_invokee(singleQueue);
+		// Create an instance of our main invokee:
+		auto mainInvokee = ray_tracing_with_shadows_and_ao_invokee(singleQueue);
+		// Create an instance of the invokee that handles our triangle mesh geometry:
+		auto triMeshGeomMgrInvokee = triangle_mesh_geometry_manager();
 		// Create another element for drawing the UI with ImGui
-		auto ui = gvk::imgui_manager(singleQueue);
+		auto imguiManagerInvokee = gvk::imgui_manager(singleQueue);
 
 		// Launch the render loop in 5.. 4.. 3.. 2.. 1.. 
 		gvk::start(
@@ -563,15 +415,15 @@ int main() // <== Starting point ==
 				// Enabling the extensions is not enough, we need to activate ray tracing features explicitly here:
 				aRayTracingFeatures.setRayTracingPipeline(VK_TRUE);
 			},
-				[](vk::PhysicalDeviceAccelerationStructureFeaturesKHR& aAccelerationStructureFeatures) {
+			[](vk::PhysicalDeviceAccelerationStructureFeaturesKHR& aAccelerationStructureFeatures) {
 				// ...and here:
 				aAccelerationStructureFeatures.setAccelerationStructure(VK_TRUE);
 			},
-				// Pass our main window to render into its frame buffers:
-				mainWnd,
-				// Pass the invokees that shall be invoked every frame:
-				app, ui
-				);
+			// Pass our main window to render into its frame buffers:
+			mainWnd,
+			// Pass the invokees that shall be invoked every frame:
+			mainInvokee, triMeshGeomMgrInvokee, imguiManagerInvokee
+		);
 	}
 	catch (gvk::logic_error& e) { LOG_ERROR(std::string("Caught gvk::logic_error in main(): ") + e.what()); }
 	catch (gvk::runtime_error& e) { LOG_ERROR(std::string("Caught gvk::runtime_error in main(): ") + e.what()); }
