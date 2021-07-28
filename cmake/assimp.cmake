7ce0b778ff27dfcf574473ea11b1743fc9c27887
Lukas Herzberger <herzberger.lukas@gmail.com>
2021-07-28 15:12:01 +0200

    add option to control if assimp and glfw should be built as static or shared libraries

--- cmake/assimp.cmake
+++ cmake/assimp.cmake
@@ -2,11 +2,6 @@ cmake_minimum_required(VERSION 3.14)
 
 include(FetchContent)
 
-# Move this to top level, because it affects assimp & glfw
-set(BUILD_SHARED_LIBS OFF CACHE INTERNAL "Build static libraries")
-
-#option(BUILD_SHARED_LIBS "Build package with shared libraries." OFF)
-
 FetchContent_Declare(
         assimp
         GIT_REPOSITORY      https://github.com/assimp/assimp.git
