95810e01b7b4e5ed18dbb2d0f30ebc3a7f3343aa
lHerzberger <muskat.rabe+github@hotmail.com>
2021-07-29 11:53:22 +0200

    fix windows cmake build (release only)

--- cmake/assimp.cmake
+++ cmake/assimp.cmake
@@ -2,6 +2,10 @@ cmake_minimum_required(VERSION 3.14)
 
 include(FetchContent)
 
+# TODO: add option to use local build of assimp
+#  this would need to set assimp_SOURCE_DIR (e.g. to external/universal)
+#  and to set assimp::assimp to the .so/.dll
+
 set(ASSIMP_BUILD_ASSIMP_TOOLS OFF)
 set(ASSIMP_BUILD_TESTS OFF)
 set(INJECT_DEBUG_POSTFIX OFF)
