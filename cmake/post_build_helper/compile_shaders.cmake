af6d1333a285dd26ea69892ac7d0c742ceba61c4
lHerzberger <muskat.rabe+github@hotmail.com>
2021-08-10 21:47:30 +0200

    fix shader compilation such that the original directory structure is preserved in the spirv output directory

--- cmake/post_build_helper/compile_shaders.cmake
+++ cmake/post_build_helper/compile_shaders.cmake
@@ -1,20 +1,19 @@
 function(get_shaders shaders glslDirectory)
     # TODO: check if other file extensions should be included (e.g. .glsl)
-    # TODO: check if directories should be checked
-    file(GLOB_RECURSE glslSourceFiles
-            "${glslDirectory}/*.vert"
-            "${glslDirectory}/*.tesc"
-            "${glslDirectory}/*.tese"
-            "${glslDirectory}/*.geom"
-            "${glslDirectory}/*.frag"
-            "${glslDirectory}/*.comp"
-            "${glslDirectory}/*.rgen"
-            "${glslDirectory}/*.rint"
-            "${glslDirectory}/*.rahit"
-            "${glslDirectory}/*.rchit"
-            "${glslDirectory}/*.rmiss"
-            "${glslDirectory}/*.rcall"
-            "${glslDirectory}/*.mesh") # TODO: whats the common file extension for mesh shaders?
+    file(GLOB_RECURSE glslSourceFiles RELATIVE ${glslDirectory}
+        "*.vert"
+        "*.tesc"
+        "*.tese"
+        "*.geom"
+        "*.frag"
+        "*.comp"
+        "*.rgen"
+        "*.rint"
+        "*.rahit"
+        "*.rchit"
+        "*.rmiss"
+        "*.rcall"
+        "*.mesh")  # TODO: whats the common file extension for mesh shaders?
     set(${shaders} ${glslSourceFiles} PARENT_SCOPE)
 endfunction(get_shaders)
 
@@ -28,16 +27,17 @@ function(make_shader_target shaderTarget target glslDirectory spvDirectory)
     get_shaders(glslSourceFiles ${glslDirectory})
 
     foreach(glslShader ${glslSourceFiles})
+        set(spvShaderFile "${spvDirectory}/${glslShader}.spv")
+        set(glslShaderAbsolute "${glslDirectory}/${glslShader}")
         get_filename_component(glslShaderFileName ${glslShader} NAME)
-        set(spvShaderFile "${spvDirectory}/${glslShaderFileName}.spv")
+        get_filename_component(spvShaderDirectory ${spvShaderFile} DIRECTORY)
         add_custom_command(
-                OUTPUT ${spvShaderFile}
-                COMMAND ${CMAKE_COMMAND} -E make_directory ${spvDirectory}
-                COMMAND ${glslangValidator}
-                    --target-env vulkan1.2 -V ${glslShader} -o ${spvShaderFile}
-                DEPENDS ${glslShader}
-                COMMENT "Compiling GLSL shader ${glslShaderFileName} to SPIR-V shader ${spvShaderFile}"
-        )
+            OUTPUT ${spvShaderFile}
+            COMMAND ${CMAKE_COMMAND} -E make_directory ${spvShaderDirectory}
+            COMMAND ${glslangValidator}
+                --target-env vulkan1.2 -V ${glslShaderAbsolute} -o ${spvShaderFile}
+            DEPENDS ${glslShaderAbsolute}
+            COMMENT "Compiling GLSL shader ${glslShaderFileName} to SPIR-V shader ${spvShaderFile}")
         list(APPEND spvShaders ${spvShaderFile})
         message(STATUS "Added GLSL shader ${glslShaderFileName} to dependencies of target ${target}")
     endforeach(glslShader)
@@ -57,10 +57,11 @@ function(compile_shaders targetName glslDirectory spvDirectory)
     get_shaders(glslSourceFiles ${glslDirectory})
 
     foreach(glslShader ${glslSourceFiles})
+        set(spvShaderFile "${spvDirectory}/${glslShader}.spv")
         get_filename_component(glslShaderFileName ${glslShader} NAME)
-        set(spvShaderFile "${spvDirectory}/${glslShaderFileName}.spv")
+        get_filename_component(spvShaderDirectory ${spvShaderFile} DIRECTORY)
         execute_process(COMMAND ${glslangValidator}
-                --target-env vulkan1.2 -V ${glslShader} -o ${spvShaderFile})
+            --target-env vulkan1.2 -V "${glslDirectory}/${glslShader}" -o ${spvShaderFile})
         message(STATUS "Compiled GLSL shader ${glslShaderFileName} to SPIR-V shader ${spvShaderFile}")
     endforeach(glslShader)
 endfunction()
