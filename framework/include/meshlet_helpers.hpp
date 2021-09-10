62bfa7097e143e63d59ea1f085befb2b89a26399
JakobPer <JakobPer@protonmail.com>
2021-09-10 13:02:54 +0200

    added combineSubmeshes flag for the meshlet generation

--- framework/include/meshlet_helpers.hpp
+++ framework/include/meshlet_helpers.hpp
@@ -6,20 +6,20 @@ namespace gvk
 	struct meshlet
 	{
 		gvk::model mModel;
-		mesh_index_t mMeshIndex;
-		std::vector<glm::vec3> vertices;
-		std::vector<uint32_t> indices;
+		std::optional<mesh_index_t> mMeshIndex;
+		std::vector<glm::vec3> mVertices;
+		std::vector<uint32_t> mIndices;
 		uint32_t mVertexCount;
 		uint32_t mIndexCount;
 	};
 
-	static std::vector<meshlet> divide_into_meshlets(const std::vector<std::tuple<avk::owning_resource<gvk::model_t>, std::vector<mesh_index_t>>>& aModels,
-		const uint32_t aMaxVertices = 64, const uint32_t aMaxIndices = 378);
+	static std::vector<meshlet> divide_into_meshlets(const std::vector<std::tuple<avk::resource_ownership<gvk::model_t>, std::vector<mesh_index_t>>>& aModels,
+		const uint32_t aMaxVertices = 64, const uint32_t aMaxIndices = 378, const bool aCombineSubmeshes = true);
 
 	static std::vector<meshlet> divide_into_very_bad_meshlets(
 		const std::vector<glm::vec3>& aPositions,
 		const std::vector<uint32_t>& aIndices,
-		avk::owning_resource<gvk::model_t> aModel,
-		const mesh_index_t aMeshIndex,
+		avk::resource_ownership<gvk::model_t> aModel,
+		const std::optional<mesh_index_t> aMeshIndex,
 		const uint32_t aMaxVertices, const uint32_t aMaxIndices);
 }
--- framework/src/meshlet_helpers.cpp
+++ framework/src/meshlet_helpers.cpp
@@ -1,63 +1,78 @@
 ﻿#include <gvk.hpp>
 #include "meshlet_helpers.hpp"
 
-static std::vector<gvk::meshlet> gvk::divide_into_meshlets(const std::vector<std::tuple<avk::owning_resource<gvk::model_t>, std::vector<mesh_index_t>>>& aModels,
-	const uint32_t aMaxVertices, const uint32_t aMaxIndices)
+namespace gvk
 {
-	std::vector<meshlet> meshlets;
-	for(auto& pair : aModels)
+	static std::vector<meshlet> divide_into_meshlets(const std::vector<std::tuple<avk::resource_ownership<model_t>, std::vector<mesh_index_t>>>& aModels,
+		const uint32_t aMaxVertices, const uint32_t aMaxIndices, const bool aCombineSubmeshes)
 	{
-		auto& model = std::get<avk::owning_resource<gvk::model_t>>(pair);
-		auto& meshIndices = std::get<std::vector<mesh_index_t>>(pair);
-		for (const auto meshIndex : meshIndices) 
-		{
-			auto vertices = model.get().positions_for_mesh(meshIndex);
-			auto indices = model.get().indices_for_mesh<uint32_t>(meshIndex);
-			// default to very bad meshlet generation
-			auto tmpMeshlets = divide_into_very_bad_meshlets(vertices, indices, model, meshIndex, aMaxVertices, aMaxIndices);
-			// append to meshlets
-			meshlets.insert(std::end(meshlets), std::begin(tmpMeshlets), std::end(tmpMeshlets));
+		std::vector<meshlet> meshlets;
+		for (auto& pair : aModels) {
+			auto tmpModel = std::get<avk::resource_ownership<model_t>>(pair);
+			auto& meshIndices = std::get<std::vector<mesh_index_t>>(pair);
+			model ownedModel = tmpModel.own();
+
+			if (aCombineSubmeshes) {
+				auto [vertices, indices] = get_vertices_and_indices(std::vector({ std::make_tuple(avk::const_referenced(ownedModel), meshIndices) }));
+				// default to very bad meshlet generation
+				auto tmpMeshlets = divide_into_very_bad_meshlets(vertices, indices, avk::owned(ownedModel), std::nullopt, aMaxVertices, aMaxIndices);
+				// append to meshlets
+				meshlets.insert(std::end(meshlets), std::begin(tmpMeshlets), std::end(tmpMeshlets));
+			}
+			else {
+				for (const auto meshIndex : meshIndices)
+				{
+					auto vertices = ownedModel.get().positions_for_mesh(meshIndex);
+					auto indices = ownedModel.get().indices_for_mesh<uint32_t>(meshIndex);
+					// default to very bad meshlet generation
+					auto tmpMeshlets = divide_into_very_bad_meshlets(vertices, indices, avk::owned(ownedModel), meshIndex, aMaxVertices, aMaxIndices);
+					// append to meshlets
+					meshlets.insert(std::end(meshlets), std::begin(tmpMeshlets), std::end(tmpMeshlets));
+				}
+			}
 		}
-	}
-	return meshlets;
-}
-
-std::vector<gvk::meshlet> gvk::divide_into_very_bad_meshlets(const std::vector<glm::vec3>& aPositions,
-	const std::vector<uint32_t>& aIndices,
-	avk::owning_resource<gvk::model_t> aModel,
-	const mesh_index_t aMeshIndex,
-	const uint32_t aMaxVertices, const uint32_t aMaxIndices)
-{
-	std::vector<meshlet> veryBadMeshlets;
-
-	int vertexIndex = 0;
-	const int numIndices = static_cast<int>(aIndices.size());
-	while (vertexIndex < numIndices) {
-		auto& ml = veryBadMeshlets.emplace_back();
-		ml.mVertexCount = 0u;
-		ml.vertices.reserve(aMaxVertices);
-		ml.indices.reserve(aMaxIndices);
-		int meshletVertexIndex = 0;
-		// this simple algorithm duplicates vertices, therefore vertexCount == indexCount
-		while (meshletVertexIndex < aMaxVertices - 3 && ml.mVertexCount < aMaxIndices - 3 && vertexIndex + meshletVertexIndex < numIndices) {
-			ml.indices[meshletVertexIndex / 3 + 0] = meshletVertexIndex + 0;
-			ml.indices[meshletVertexIndex / 3 + 1] = meshletVertexIndex + 1;
-			ml.indices[meshletVertexIndex / 3 + 2] = meshletVertexIndex + 2;
-			ml.vertices[meshletVertexIndex + 0] = aPositions[aIndices[vertexIndex + meshletVertexIndex + 0]];
-			ml.vertices[meshletVertexIndex + 1] = aPositions[aIndices[vertexIndex + meshletVertexIndex + 1]];
-			ml.vertices[meshletVertexIndex + 2] = aPositions[aIndices[vertexIndex + meshletVertexIndex + 2]];
-			ml.mVertexCount += 3u;
-
-			meshletVertexIndex += 3;
-		}
-		ml.mIndexCount = ml.mVertexCount;
-		// resize to actual size
-		ml.vertices.resize(ml.mVertexCount);
-		ml.vertices.shrink_to_fit();
-		ml.indices.resize(ml.mVertexCount);
-		ml.indices.shrink_to_fit();
-		vertexIndex += meshletVertexIndex;
+		return meshlets;
 	}
 
-	return veryBadMeshlets;
+	std::vector<meshlet> divide_into_very_bad_meshlets(const std::vector<glm::vec3>& aPositions,
+		const std::vector<uint32_t>& aIndices,
+		avk::resource_ownership<model_t> aModel,
+		const std::optional<mesh_index_t> aMeshIndex,
+		const uint32_t aMaxVertices, const uint32_t aMaxIndices)
+	{
+		std::vector<meshlet> veryBadMeshlets;
+
+		int vertexIndex = 0;
+		const int numIndices = static_cast<int>(aIndices.size());
+		while (vertexIndex < numIndices) {
+			auto& ml = veryBadMeshlets.emplace_back();
+			ml.mVertexCount = 0u;
+			ml.mVertices.reserve(aMaxVertices);
+			ml.mIndices.reserve(aMaxIndices);
+			int meshletVertexIndex = 0;
+			// this simple algorithm duplicates vertices, therefore vertexCount == indexCount
+			while (meshletVertexIndex < aMaxVertices - 3 && ml.mVertexCount < aMaxIndices - 3 && vertexIndex + meshletVertexIndex < numIndices) {
+				ml.mIndices[meshletVertexIndex / 3 + 0] = meshletVertexIndex + 0;
+				ml.mIndices[meshletVertexIndex / 3 + 1] = meshletVertexIndex + 1;
+				ml.mIndices[meshletVertexIndex / 3 + 2] = meshletVertexIndex + 2;
+				ml.mVertices[meshletVertexIndex + 0] = aPositions[aIndices[vertexIndex + meshletVertexIndex + 0]];
+				ml.mVertices[meshletVertexIndex + 1] = aPositions[aIndices[vertexIndex + meshletVertexIndex + 1]];
+				ml.mVertices[meshletVertexIndex + 2] = aPositions[aIndices[vertexIndex + meshletVertexIndex + 2]];
+				ml.mVertexCount += 3u;
+
+				meshletVertexIndex += 3;
+			}
+			ml.mIndexCount = ml.mVertexCount;
+			// resize to actual size
+			ml.mVertices.resize(ml.mVertexCount);
+			ml.mVertices.shrink_to_fit();
+			ml.mIndices.resize(ml.mVertexCount);
+			ml.mIndices.shrink_to_fit();
+			ml.mModel = aModel.own();
+			ml.mMeshIndex = aMeshIndex;
+			vertexIndex += meshletVertexIndex;
+		}
+
+		return veryBadMeshlets;
+	}
 }
