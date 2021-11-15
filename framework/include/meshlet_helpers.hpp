0b1cda46672b08bda55230c9e13069740022077f
JakobPer <JakobPer@protonmail.com>
2021-11-15 12:31:54 +0100

    moved method

--- framework/include/meshlet_helpers.hpp
+++ framework/include/meshlet_helpers.hpp
@@ -37,6 +37,72 @@ namespace gvk
 		std::optional<mesh_index_t> aMeshIndex,
 		uint32_t aMaxVertices, uint32_t aMaxIndices);
 
+	/** Divides the given vertex and index buffer into meshlets using the given callback function.
+	 *  @param	aVertices			The vertex buffer.
+	 *  @param	aIndices			The index buffer.
+	 *  @param	aModel				The model these buffers belong to.
+	 *	@param	aMeshIndex			The optional mesh index of the mesh these buffers belong to.
+	 *  @param	aMeshletDivision	Callback used to divide meshes into meshlets with a maximum number of vertices and indices each.
+	 *								It can either receive the vertices and indices or just the indices depending on your specific needs.
+	 *								Additionally it provides the model and an optional mesh index if more data is needed. If no mesh index is provided then the meshes were combined beforehand.
+	 *								Must not grab ownership of the model, the model will be assigned to the meshlets after its execution.\n\n
+	 *								The callback must follow a specific declaration schema, optional parameters can be omitted, but all of them need to be provided in the following order:
+	 *								 - const std::vector<glm::vec3>& tVertices:		optional	The vertices of the mesh or combined meshes of the model
+	 *								 - const std::vector<uint32_t>& tIndices:  		mandatory	The indices of the mesh or combined meshes of the model
+	 *								 - const model_t& tModel:						mandatory	The model these meshlets are generated from
+	 *								 - std::optional<mesh_index_t> tMeshIndex: 		mandatory	The optional mesh index. If no value is provided, it means the meshes of the model are combined into a single vertex and index buffer
+	 *								 - uint32_t tMaxVertices:						mandatory	The maximum number of vertices that are allowed in a single meshlet.
+	 *								 - uint32_t tMaxIndices:						mandatory	The maximum number of indices that are allowed in a single meshlet.
+	 *								Return value:
+	 *								 - std::vector<meshlet>										The generated meshlets.\n\n
+	 *								Example:
+	 *								@code
+	 *								[](const std::vector<glm::vec3>& tVertices, const std::vector<uint32_t>& aIndices,
+	 *											const model_t& aModel, std::optional<mesh_index_t> aMeshIndex,
+	 *											uint32_t aMaxVertices, uint32_t aMaxIndices) {
+	 *									std::vector<gvk::meshlet> generatedMeshlets;
+	 *									// Do your meshlet division here
+	 *									return generatedMeshlets;
+	 *								}
+	 *								@endcode
+	 *	@param	aMaxVertices		The maximum number of vertices of a meshlet.
+	 *	@param	aMaxIndices			The maximum number of indices of a meshlet.
+	 */
+	template <typename F>
+	std::vector<meshlet> divide_indexed_geometry_into_meshlets(
+		const std::vector<glm::vec3>& aVertices,
+		const std::vector<uint32_t>& aIndices,
+		avk::resource_ownership<gvk::model_t> aModel,
+		const std::optional<mesh_index_t> aMeshIndex,
+		const uint32_t aMaxVertices, const uint32_t aMaxIndices,
+		F aMeshletDivision)
+	{
+		std::vector<meshlet> generatedMeshlets;
+		auto ownedModel = aModel.own();
+		ownedModel.enable_shared_ownership();
+
+		if constexpr (std::is_assignable_v<std::function<std::vector<meshlet>(const std::vector<uint32_t>& tIndices, const model_t& tModel, std::optional<mesh_index_t> tMeshIndex, uint32_t tMaxVertices, uint32_t tMaxIndices)>, decltype(aMeshletDivision)>) {
+			generatedMeshlets = aMeshletDivision(aIndices, ownedModel.get(), aMeshIndex, aMaxVertices, aMaxIndices);
+		}
+		else if constexpr (std::is_assignable_v<std::function<std::vector<meshlet>(const std::vector<glm::vec3>& tVertices, const std::vector<uint32_t>& tIndices, const model_t& tModel, std::optional<mesh_index_t> tMeshIndex, uint32_t tMaxVertices, uint32_t tMaxIndices)>, decltype(aMeshletDivision)>) {
+			generatedMeshlets = aMeshletDivision(aVertices, aIndices, ownedModel.get(), aMeshIndex, aMaxVertices, aMaxIndices);
+		}
+		else {
+#if defined(_MSC_VER) && defined(__cplusplus)
+			static_assert(false);
+#else
+			assert(false);
+#endif
+			throw avk::logic_error("No compatible lambda has been passed to divide_into_meshlets.");
+		}
+
+		for (auto& meshlet : generatedMeshlets)
+		{
+			meshlet.mModel = ownedModel;
+		}
+
+		return generatedMeshlets;
+	}
 
 	/** Divides the given models into meshlets using the default implementation divide_into_meshlets_simple.
 	 *  @param	aModelsAndMeshletIndices				All the models and associated meshes that should be divided into meshlets.
@@ -103,73 +169,6 @@ namespace gvk
 			}
 		}
 		return meshlets;
-	}
-
-	/** Divides the given vertex and index buffer into meshlets using the given callback function.
-	 *  @param	aVertices			The vertex buffer.
-	 *  @param	aIndices			The index buffer.
-	 *  @param	aModel				The model these buffers belong to.
-	 *	@param	aMeshIndex			The optional mesh index of the mesh these buffers belong to.
-	 *  @param	aMeshletDivision	Callback used to divide meshes into meshlets with a maximum number of vertices and indices each.
-	 *								It can either receive the vertices and indices or just the indices depending on your specific needs.
-	 *								Additionally it provides the model and an optional mesh index if more data is needed. If no mesh index is provided then the meshes were combined beforehand.
-	 *								Must not grab ownership of the model, the model will be assigned to the meshlets after its execution.\n\n
-	 *								The callback must follow a specific declaration schema, optional parameters can be omitted, but all of them need to be provided in the following order:
-	 *								 - const std::vector<glm::vec3>& tVertices:		optional	The vertices of the mesh or combined meshes of the model
-	 *								 - const std::vector<uint32_t>& tIndices:  		mandatory	The indices of the mesh or combined meshes of the model
-	 *								 - const model_t& tModel:						mandatory	The model these meshlets are generated from
-	 *								 - std::optional<mesh_index_t> tMeshIndex: 		mandatory	The optional mesh index. If no value is provided, it means the meshes of the model are combined into a single vertex and index buffer
-	 *								 - uint32_t tMaxVertices:						mandatory	The maximum number of vertices that are allowed in a single meshlet.
-	 *								 - uint32_t tMaxIndices:						mandatory	The maximum number of indices that are allowed in a single meshlet.
-	 *								Return value:
-	 *								 - std::vector<meshlet>										The generated meshlets.\n\n
-	 *								Example:
-	 *								@code
-	 *								[](const std::vector<glm::vec3>& tVertices, const std::vector<uint32_t>& aIndices,
-	 *											const model_t& aModel, std::optional<mesh_index_t> aMeshIndex,
-	 *											uint32_t aMaxVertices, uint32_t aMaxIndices) {
-	 *									std::vector<gvk::meshlet> generatedMeshlets;
-	 *									// Do your meshlet division here
-	 *									return generatedMeshlets;
-	 *								}
-	 *								@endcode
-	 *	@param	aMaxVertices		The maximum number of vertices of a meshlet.
-	 *	@param	aMaxIndices			The maximum number of indices of a meshlet.
-	 */
-	template <typename F>
-	std::vector<meshlet> divide_indexed_geometry_into_meshlets(
-		const std::vector<glm::vec3>& aVertices,
-		const std::vector<uint32_t>& aIndices,
-		avk::resource_ownership<gvk::model_t> aModel,
-		const std::optional<mesh_index_t> aMeshIndex,
-		const uint32_t aMaxVertices, const uint32_t aMaxIndices,
-		F aMeshletDivision)
-	{
-		std::vector<meshlet> generatedMeshlets;
-		auto ownedModel = aModel.own();
-		ownedModel.enable_shared_ownership();
-
-		if constexpr (std::is_assignable_v<std::function<std::vector<meshlet>(const std::vector<uint32_t>& tIndices, const model_t& tModel, std::optional<mesh_index_t> tMeshIndex, uint32_t tMaxVertices, uint32_t tMaxIndices)>, decltype(aMeshletDivision)>) {
-			generatedMeshlets = aMeshletDivision(aIndices, ownedModel.get(), aMeshIndex, aMaxVertices, aMaxIndices);
-		}
-		else if constexpr (std::is_assignable_v<std::function<std::vector<meshlet>(const std::vector<glm::vec3>& tVertices, const std::vector<uint32_t>& tIndices, const model_t& tModel, std::optional<mesh_index_t> tMeshIndex, uint32_t tMaxVertices, uint32_t tMaxIndices)>, decltype(aMeshletDivision)>) {
-			generatedMeshlets = aMeshletDivision(aVertices, aIndices, ownedModel.get(), aMeshIndex, aMaxVertices, aMaxIndices);
-		}
-		else {
-#if defined(_MSC_VER) && defined(__cplusplus)
-			static_assert(false);
-#else
-			assert(false);
-#endif
-			throw avk::logic_error("No compatible lambda has been passed to divide_into_meshlets.");
-		}
-
-		for (auto& meshlet : generatedMeshlets)
-		{
-			meshlet.mModel = ownedModel;
-		}
-
-		return generatedMeshlets;
 	}
 }
 
