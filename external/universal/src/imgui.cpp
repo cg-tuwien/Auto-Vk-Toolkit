e139963bc0ee64b7e2495a2795922236a05b815a
Johannes Unterguggenberger <johannes.unterguggenberger@gmail.com>
2023-05-04 08:40:28 +0200

    Replaced imgui.cpp with original file once again

--- external/universal/src/imgui.cpp
+++ external/universal/src/imgui.cpp
@@ -888,9 +888,9 @@ CODE
 #define IMGUI_DEFINE_MATH_OPERATORS
 #endif
 
-#include <imgui.h>
+#include "imgui.h"
 #ifndef IMGUI_DISABLE
-#include <imgui_internal.h>
+#include "imgui_internal.h"
 
 // System includes
 #include <stdio.h>      // vsnprintf, sscanf, printf
