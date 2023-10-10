333f0ea0ce7a50be81073620692b8e851c6cd201
Johannes Unterguggenberger <johannes.unterguggenberger@gmail.com>
2023-10-10 13:46:14 +0200

    Using JetBrainsMono-Regular.ttf as UI font and also accounting for display's content scale through GLFW

---
+++ auto_vk_toolkit/include/JetBrainsMono-Regular.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace avk
+{
+	namespace JetBrainsMono_Regular
+    {
+		extern const char* get_font_name();
+	    extern std::tuple<size_t, uint8_t*> get_size_and_bytes();
+	}
+}
