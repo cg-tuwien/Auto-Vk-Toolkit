f5d429f98341003de9cc3ceb87a322deee1d881e
Johannes Unterguggenberger <johannes.unterguggenberger@gmail.com>
2024-04-12 14:10:19 +0200

    Dynamic rendering (#186)
    * Added functionality to enable VK_KHR_Dynamic_rendering extension and updated samples to match AutoVK changes
    
    * Changed Auto-vk submodule version to match dynamic_rendering branch
    
    * Added new example showcasing (and testing) dynamic rendering
    
    * Modified Cmake to also include new example
    
    * Bump submodule version to fix Vulkan 1.2 build
    
    * Apply suggestions from code review
    
    - local variables to lowerCamelCase
    - remove artefacts of old versions
    - missing newline at the end of shaderfiles
    
    Co-authored-by: Johannes Unterguggenberger <johannes.unterguggenberger@gmail.com>
    
    * fix the submodule head to be on the dynamic rendering branch
    
    * Add fixes to issues raised in pr review:
    - Changed the way split is defined
    - Deleted duplicit files for visual studio examples
    - Removed redundant check in dynamic rendering support
    - Renamed "renderpass" to "draw"/"pass"
    
    * Bump submodule to be up to date with recent changes and fix examples to use renderpass_reference() again instead of renderpass_pointer()
    
    * Fixed a bug where the edges of draws would not be drawn in black + added MSAA into the example
    
    * Changed the example to work with the newest API changes
    
    * bump submodule
    
    * Bumped submodule after dynamic_rendering merge in Auto-Vk
    
    * Testing with Vulkan SDK 1.3.216 instead of 1.3.204
    
    ---------
    
    Co-authored-by: MatejSakmary <matej.hydro@gmail.com>

---
+++ visual_studio/examples/dynamic_rendering/cg_stdafx.cpp
@@ -0,0 +1,8 @@
+// cg_stdafx.cpp : source file that includes just the standard includes
+// cg_stdafx.pch will be the pre-compiled header
+// cg_stdafx.obj will contain the pre-compiled type information
+
+#include "cg_stdafx.hpp"
+
+// TODO: reference any additional headers you need in cg_stdafx.hpp
+// and not in this file
