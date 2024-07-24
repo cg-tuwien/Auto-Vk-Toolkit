a72a6f95ff641132de1a354ad7087592831a1324
Johannes Unterguggenberger <johannes.unterguggenberger@gmail.com>
2024-07-24 13:07:19 +0200

    Added the CMake-generated config.h file

--- external/universal/include/assimp/config.h
+++ external/universal/include/assimp/config.h
@@ -1163,6 +1163,6 @@ enum aiComponent
  * Property type: Bool. Default value: undefined.
  */
 
-#cmakedefine ASSIMP_DOUBLE_PRECISION 1
+/* #undef ASSIMP_DOUBLE_PRECISION */
 
 #endif // !! AI_CONFIG_H_INC
